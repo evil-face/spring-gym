4516accda7352fffdaf52f13b33a9de8320c2f81
evil_face <den.bragin@gmail.com>
2023-09-09 21:28:39 +0600

    feature: add Training type entity

---
+++ src/main/java/epam/xstack/model/TrainingType.java
@@ -0,0 +1,50 @@
+package epam.xstack.model;
+
+import java.util.Objects;
+
+public final class TrainingType implements GymEntity {
+    private String id;
+    private String trainingTypeName;
+
+    public TrainingType(String id, String trainingTypeName) {
+        this.id = id;
+        this.trainingTypeName = trainingTypeName;
+    }
+
+    public TrainingType() {
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTrainingTypeName() {
+        return trainingTypeName;
+    }
+
+    public void setTrainingTypeName(String trainingTypeName) {
+        this.trainingTypeName = trainingTypeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TrainingType that = (TrainingType) o;
+        return Objects.equals(id, that.id) && Objects.equals(trainingTypeName, that.trainingTypeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, trainingTypeName);
+    }
+}
