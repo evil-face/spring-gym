abce164b48ded5699b080f9018d5c7196fa998ef
evil_face <den.bragin@gmail.com>
2023-09-11 20:07:16 +0600

    test: add UserService tests

---
+++ src/test/java/epam/xstack/UserServiceTest.java
@@ -0,0 +1,64 @@
+package epam.xstack;
+
+import epam.xstack.dao.UserDAO;
+import epam.xstack.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    private static final int PASSWORD_LENGTH = 10;
+    @InjectMocks
+    public UserService userService;
+    @Mock
+    public UserDAO userDAO;
+
+    @Test
+    public void testGenerateId() {
+        String actual = userService.generateId();
+
+        Assertions.assertDoesNotThrow(() -> UUID.fromString(actual));
+    }
+
+    @Test
+    public void testGenerateUsername() {
+        String expected = "john.wick";
+        when(userDAO.existsByUsername(expected)).thenReturn(false);
+        String actual = userService.generateUsername(" john ", "wi ck ");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGenerateUsernameDuplicate() {
+        String expected = "john.wick";
+        Mockito.doReturn(true).when(userDAO).existsByUsername(expected);
+        String actual = userService.generateUsername(" john ", "wi ck ");
+
+        assertThat(actual).isEqualTo(expected + "1");
+    }
+
+    @Test
+    public void testGenerateUsernameBadInput() {
+        String expected = "john.wick";
+        Mockito.doReturn(false).when(userDAO).existsByUsername(expected);
+        String actual = userService.generateUsername("1john2", "3wick444");
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void generatePassword() {
+        String actual = userService.generatePassword();
+        assertThat(actual).hasSize(PASSWORD_LENGTH);
+    }
+}
