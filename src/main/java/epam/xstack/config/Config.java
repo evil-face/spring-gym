fd05715e2a8e0a4053ca432c69c3587eabbcbcbe
evil_face <den.bragin@gmail.com>
2023-09-27 18:02:42 +0600

    build: add Spring WebMVC

--- src/main/java/epam/xstack/config/Config.java
+++ src/main/java/epam/xstack/config/Config.java
@@ -1,6 +1,8 @@
 package epam.xstack.config;
 
 import epam.xstack.interceptor.RequestLoggingInterceptor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
