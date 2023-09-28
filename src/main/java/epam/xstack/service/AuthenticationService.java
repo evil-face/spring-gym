28ba1958430f6bdf4056de3960d2119c985142a4
evil_face <den.bragin@gmail.com>
2023-09-28 17:45:43 +0600

    feature: integrate AuthenticationService with REST controllers

--- src/main/java/epam/xstack/service/AuthenticationService.java
+++ src/main/java/epam/xstack/service/AuthenticationService.java
@@ -1,28 +1,45 @@
 package epam.xstack.service;
 
+import epam.xstack.exception.UnauthorizedException;
 import epam.xstack.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.AuthenticationException;
 import java.util.Optional;
 
 @Service
 public final class AuthenticationService {
     private final UserService userService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     public AuthenticationService(UserService userService) {
         this.userService = userService;
     }
 
-    public boolean authenticate(String username, String password) {
-        Optional<User> user = userService.findByUsername(username);
+    public boolean authenticate(String txID, String username, String password) {
+        Optional<User> user = userService.findByUsername(txID, username);
 
         if (user.isPresent()) {
             User principal = user.get();
 
-            return principal.getUsername().equals(username)
-                    && principal.getPassword().equals(password);
+            if (principal.getUsername().equals(username) && principal.getPassword().equals(password)) {
+                return true;
+            } else {
+                throw new UnauthorizedException(txID);
+            }
+        } else {
+            LOGGER.warn("TX ID: {} — No user was found to authenticate with username {}", txID, username);
+            throw new UnauthorizedException(txID);
+        }
+    }
+
+    public boolean updatePassword(String txID, String username, String oldPassword, String newPassword) throws AuthenticationException {
+        if (authenticate(txID, username, oldPassword)) {
+            return userService.updatePassword(txID, username, newPassword);
         }
 
         return false;
