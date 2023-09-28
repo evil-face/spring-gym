ed77571ceb14bb98838a6c404d5258e2aca55089
evil_face <den.bragin@gmail.com>
2023-09-28 17:49:38 +0600

    feature: add new Exceptions for REST controllers

---
+++ src/main/java/epam/xstack/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package epam.xstack.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
