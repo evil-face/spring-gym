01441df41780d78d09e3d9bda3d7d1c554b90d23
evil_face <den.bragin@gmail.com>
2023-09-29 12:32:26 +0600

    chore: remove unnecessary imports

--- src/main/java/epam/xstack/service/AuthenticationService.java
+++ src/main/java/epam/xstack/service/AuthenticationService.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.naming.AuthenticationException;
 import java.util.Optional;
 
 @Service
